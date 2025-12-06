fca386bbf0056dc47487ee0868738b5f7dc49665
Abdullah Özhanlı <abdullahozhanli292@gmail.com>
2025-12-06 23:59:26 +0300

    temel sınıf ve proje yapısı oluşturuldu

--- app/modules/module_3/base.py
+++ app/modules/module_3/base.py
@@ -1,11 +1,22 @@
-# app/modules/module_3/base.py
 from abc import ABC, abstractmethod
+from datetime import datetime
 
-class BaseClass3(ABC):
-    def __init__(self, parameter: str):
-        self.base3Attribute = parameter
+#projeye özel hata sınıfı   
+class TurnuvaHatasi(Exception):
+    pass    
 
-    @abstractmethod
-    def method3(self) -> None:
-        """metod tanimi."""
-        pass
+class MacBase(ABC):
+    toplam_mac_sayisi = 0  
+    gecerli_durumlar = ['planlandi', 'devam_ediyor', 'tamamlandi', 'iptal_edildi', 'ertelendi']
+
+    def __init__(self, ev_sahibi, deplasman, tarih_saat, mac_id):
+        self.ev_sahibi = ev_sahibi
+        self.deplasman = deplasman
+        self.tarih_saat = tarih_saat
+        self.mac_id = mac_id
+        self.durum = 'planlandi'
+        self.skor_ev = 0
+        self.skor_deplasman = 0
+        self.skor_girildimi = False
+        self. konum = "Ana Stadyum"
+        self.hakem = "Atanmadı"
